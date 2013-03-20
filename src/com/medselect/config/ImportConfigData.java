95b79df1d8c2164531ffc7128a82cb8f320941c6
karlosmid <karlosmid@78e82c56-2ac9-48e7-9d20-0400322f7d95>
2013-03-20 10:31:04 +0000

    config import

--- src/com/medselect/config/ImportConfigData.java
+++ src/com/medselect/config/ImportConfigData.java
@@ -16,6 +16,8 @@ import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import java.util.logging.Logger;
 
+import com.medselect.config.ConfigManager;
+import com.medselect.common.ReturnMessage;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
@@ -46,9 +48,37 @@ public class ImportConfigData extends HttpServlet {
       response.sendRedirect("/server/include/config-import-data.jsp?res=failed");
     } else {
       // Now read from the file using the Blobstore API
-      // TODO: Karlo, implement this
+      String importedData = new String(blobstoreService.fetchData(blobKey, 0, BlobstoreService.MAX_BLOB_FETCH_SIZE-1));
       boolean importSuccessful = true;
-     
+      try{
+        JSONArray configsAsJsonArray = new JSONArray(importedData);
+        JSONObject jsonElement = null;
+        JSONArray listTitles = null;
+        JSONArray listSpecialties = null;
+        ConfigManager configManager = new ConfigManager();
+        for(int index = 0;index < configsAsJsonArray.length(); index++){
+          jsonElement = configsAsJsonArray.getJSONObject(index);
+          Map<String,String> configParams= new HashMap<String,String>();
+          
+          configParams.put("text", jsonElement.getString("text"));
+          configParams.put("description", jsonElement.getString("description"));
+          configParams.put("imageid", jsonElement.getString("valueImageId"));
+          configParams.put("user", jsonElement.getString("modifiedBy"));
+          configParams.put("modifyDate", jsonElement.getString("modifyDate"));
+          configParams.put("name", jsonElement.getString("name"));
+          configParams.put("value", jsonElement.getString("value"));
+          configParams.put("appCode", jsonElement.getString("appCode"));
+          configParams.put("createDate", jsonElement.getString("createDate"));
+          configParams.put("createdBy", jsonElement.getString("createdBy"));
+          ReturnMessage responseForInsert = configManager.insertConfigValue(configParams);
+          if (responseForInsert.getStatus().equals("FAILURE")){
+            importSuccessful = false;
+          }
+        }
+      } catch(JSONException jsonEx){
+        LOGGER.severe(jsonEx.getMessage());
+        importSuccessful = false;
+      }
       if (importSuccessful) {
         response.sendRedirect("/server/include/config-import-data.jsp?res=success");
       } else {
