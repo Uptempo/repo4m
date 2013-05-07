0658449718980869b6b016d95bfe7e02572ef4b5
mgordon <mgordon@78e82c56-2ac9-48e7-9d20-0400322f7d95>
2013-05-07 13:39:09 +0000

    Temp fixes for office time and appointment time to display correctly.  Added cleanup task to change all appointment times for an office.

--- pug-creative/uptempo-platform/trunk/src/com/medselect/appointment/AppointmentCleanupServerResource.java
+++ pug-creative/uptempo-platform/trunk/src/com/medselect/appointment/AppointmentCleanupServerResource.java
@@ -3,9 +3,12 @@
  */
 package com.medselect.appointment;
 
+import java.util.Map;
+import org.restlet.data.Form;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
 /**
@@ -17,6 +20,21 @@ public class AppointmentCleanupServerResource extends ServerResource {
   public Representation clearReservedAppointments() {
     AppointmentManager aManager = new AppointmentManager();
     aManager.resetReservedAppointments();
+    StringRepresentation sr = new StringRepresentation("SUCCESS");
+    return sr;
+  }
+  
+  @Post
+  public Representation shiftAppointments(Representation apptValue) {
+    Form sForm = new Form(apptValue);
+ 
+    //*** Get the office key string and shift value.
+    Map<String, String> valueMap = sForm.getValuesMap();
+    String officeKey = valueMap.get("apptOffice");
+    String offsetString = valueMap.get("offset");
+    int offset = Integer.parseInt(offsetString);
+    AppointmentManager aManager = new AppointmentManager();
+    aManager.shiftOfficeAppointmentTimes(officeKey, offset);
     StringRepresentation sr = new StringRepresentation("SUCCESS");
     return sr;
   }
